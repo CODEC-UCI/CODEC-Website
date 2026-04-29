b118e65d15b4f2280df9bcf3aca36a911290aa4b
Akriti Singh <160444391+akritis2@users.noreply.github.com>
2026-04-29 08:35:36 -0700

    Testing file path changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function() {
     <footer>
         <div class="footer-content">
             <a href="index.html">
-                <img src="images/logos/CODEC.jpg" alt="CODEC Logo" class="footer-logo">
+                <img src="src/images/logos/CODEC.jpg" alt="CODEC Logo" class="footer-logo">
             </a>
             <div class="footer-links-stack">
-                <a href="contact-us.html" class="contact-text-link">Contact Us</a>
+                <a href="src/contact-us.html" class="contact-text-link">Contact Us</a>
                 <a href="https://discord.gg/QSYSCFb" target="_blank" class="glow-link">Discord</a>
                 <a href="https://www.instagram.com/codec.uci/" target="_blank" class="glow-link">Instagram</a>
                 <a href="https://www.linkedin.com/in/codec-uci" target="_blank" class="glow-link">LinkedIn</a>
